83992de097421c629ad888c722ae78d56218339a
vrbouza <15350389+vrbouza@users.noreply.github.com>
2025-06-10 12:16:00 +0100

    Fixed small detail for wasm

--- src/graph_works.rs
+++ src/graph_works.rs
@@ -13,6 +13,8 @@ use std::{
 };
 
 // use rayon::prelude::*;
+
+#[cfg(not(feature = "wasm"))]
 use super::io_utils::*;
 
 #[cfg(not(feature = "wasm"))]
@@ -236,7 +238,7 @@ impl Assemble for BasicAsm {
         log::info!("Prop. of alone kmers: {:.1} %", (ialone as f64) / (i as f64) * 100.0);
         log::info!("Number of edges {}", (nedges as f64) / (2 as f64));
 
-        timevec.push(Instant::now());
+        // timevec.push(Instant::now());
         log::info!("Neighbours searched for in {} s", timevec.last().unwrap().duration_since(*timevec.get(timevec.len().wrapping_sub(2)).unwrap()).as_secs());
 
         indict.iter().for_each(|(h, hi)| {
